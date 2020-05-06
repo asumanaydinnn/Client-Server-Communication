727369fe7b11d8ca43705c00dafeda15db55d095
Asuman Aydın <asuman.aydin6709@gmail.com>
2020-05-06 16:33:43 +0300

    pseudo code

--- SecureClient.java
+++ SecureClient.java
@@ -3,6 +3,7 @@ import java.io.InputStream;
 import java.net.Socket;
 import java.io.*;
 import java.util.*;
+import java.lang.Object;
 
 /**
  * Programming Assignment 2
@@ -41,13 +42,7 @@ public class SecureClient {
 	public static void main(String args[]) throws IOException {
 
 		SecureClient myObje = new SecureClient();
-		myObje.version = 0;
-		String ipaddress = args[0]; // = "localhost";
-		int port = Integer.parseInt(args[1]);// = 60000;
-
-		// Connect to the server
-		socketClient = new Socket(ipaddress, port);
-		System.out.println("Connection is started.");
+		int port = Integer.parseInt(args[0]);
 
 		// to take the input from client
 		inputStream = socketClient.getInputStream();
@@ -56,192 +51,150 @@ public class SecureClient {
 		writer = new PrintWriter(socketClient.getOutputStream(), true);
 		in = new Scanner(System.in);
 
-		// USER AUTH
-		System.out.print("Username Command:");
-		String userName = in.nextLine();
-		if (userName.contains("EXIT")) {
-			try {
-				writer.close();
-				fromServer.close();
-				inputStream.close();
-				inputStreamRe.close();
+		CryptoHelper crypto = new CryptoHelper();
+
+
+		while(true) {
+
+			socketClient = new Socket("127.0.0.1", port);
+			System.out.println("Connection is started.");
+			// --- HANDSHAKE START
+			sendHELLO();
+
+			// Receive the certificate
+			byte[] cert = receiveHELLO();
+
+			// Get necessary fields from the certificate
+			byte[] signature = getSignature(cert);
+			String ca = getCA(cert);
+			byte[] serverPublicKey = getPK(cert);
+
+			// Verification is successful:
+			if (crypto.verifySignature(cert, signature, ca)) 
+				break;
+
+			// Verification fails:
+			else 
 				socketClient.close();
-				in.close();
-			} catch (IOException exc) {
-				System.out.println("Exception while exiting: " + exc.getMessage());
-			}
-			System.out.println("Connection is closed.");
-			System.exit(0);
 		}
-		userAuth = sendUserCommand(userName);
-		if (userAuth == false) {
-			try {
-				writer.close();
-				fromServer.close();
-				inputStream.close();
-				inputStreamRe.close();
-				socketClient.close();
-				in.close();
-			} catch (IOException exc) {
-				System.out.println("Exception while exiting: " + exc.getMessage());
-			}
-			System.out.println("Connection is closed.");
-			System.exit(0);
-		}
-		// PASS AUTH
-		System.out.print("Password Command:");
-		String password = in.nextLine();
-		if (password.contains("EXIT")) {
-			try {
-				writer.close();
-				fromServer.close();
-				inputStream.close();
-				inputStreamRe.close();
-				socketClient.close();
-				in.close();
-			} catch (IOException exc) {
-				System.out.println("Exception while exiting: " + exc.getMessage());
-			}
-			System.out.println("Connection is closed.");
-			System.exit(0);
-		}
-		passAuth = sendPassCommand(password);
-		if (passAuth == false) {
-			try {
-				writer.close();
-				fromServer.close();
-				inputStream.close();
-				inputStreamRe.close();
-				socketClient.close();
-				in.close();
-			} catch (IOException exc) {
-				System.out.println("Exception while exiting: " + exc.getMessage());
-			}
-			System.out.println("Connection is closed.");
-			System.exit(0);
-		}
-		String[] response = { "OK", "INVALID" };
-		System.out.println("Please enter a command to execute: ");
-		String command = in.nextLine();
-		if (command.contains("EXIT")) {
-			try {
-				writer.close();
-				fromServer.close();
-				inputStream.close();
-				inputStreamRe.close();
-				socketClient.close();
-				in.close();
-			} catch (IOException exc) {
-				System.out.println("Exception while exiting: " + exc.getMessage());
-			}
-			System.out.println("Connection is closed.");
-			System.exit(0);
-		}
-		while (!command.contains("EXIT")) {
-			// UPDATE
-			if (command.contains("UPDT")) {
-				writer.println(command);
-				String responseFromServer = null;
-				try {
-					responseFromServer = fromServer.readLine();
-					System.out.println(responseFromServer);
-				} catch (IOException exc) {
-					System.out.println("about update: " + exc.getMessage());
-				}
-				if (responseFromServer.contains(response[0]) && !responseFromServer.contains(response[1])) {
-					int versionCurrent = myObje.getVersionOfClient();
-					versionCurrent++;
-					myObje.setVersionOfClient(versionCurrent);
-				} else {
-					System.out.println("The other functions can be used.");
-				}
-			}
-			// WRITE
-			else if (command.contains("WRTE")) {
-				writer.println(command);
-				String responseFromServerWrite = null;
-				try {
-					responseFromServerWrite = fromServer.readLine();
-					System.out.println(responseFromServerWrite);
-				} catch (IOException exc) {
-					System.out.println("About write command: " + exc.getMessage());
-				}
-			}
-			// APPEND
-			else if (command.contains("APND")) {
-				writer.println(command);
-				String responseFromServerapn = null;
-				try {
-					responseFromServerapn = fromServer.readLine();
-					System.out.println(responseFromServerapn);
-				} catch (IOException exc) {
-					System.out.println("About append: " + exc.getMessage());
-				}
-			}
-			System.out.println("Please enter a command to execute: ");
-			command = in.nextLine();
-		}
-		// CLOSING OF THE PROGRAM
-		try {
-			writer.close();
-			fromServer.close();
-			inputStream.close();
-			inputStreamRe.close();
-			socketClient.close();
-			in.close();
-		} catch (IOException exc) {
-			System.out.println("Exception while exiting: " + exc.getMessage());
-		}
-		System.out.println("Connection is closed.");
-		System.exit(0);
+
+		// Create and send encrypted secret
+		int secret = crypto.generateSecret();
+		byte[] secretEncrypted = crypto.encryptSecretAsymmetric(secret, serverPublicKey);
+		sendSECRET(secretEncrypted);
+		// --- HANDSHAKE END
+
+
+		// --- AUTHENTICATION START
+		sendSTARTENC();  // Start encryption
+
+		// Send encrypted authentication info
+		byte[] authEncrypted = crypto.encryptSymmetric("bilkent cs421", secret);
+		sendAUTH(authEncrypted);
+
+		// Receive authentication response
+		byte[] data = receiveRESPONSE();
+		String response = crypto.decryptSymmetric(data, secret);
+		print(response);  // This should be "OK"
+
+		sendSTARTENC();  // End encryption
+		// --- AUTHENTICATION END
+		// --- VIEW PUBLIC POSTS START
+		sendPUBLIC();
+		byte[] data = receiveRESPONSE();
+
+		// Decode the byte array into a string & display
+		String response = decodeUS_ASCII(data);
+		print(response);
+		// --- VIEW PUBLIC POSTS END
+
+
+		// --- VIEW PRIVATE MESSAGES START
+		sendSTARTENC();  // Start encryption
+		sendPRIVATE();
+
+		// Receive, decrypt & display
+		byte[] data = receiveRESPONSE();
+		String response = crypto.decryptSymmetric(data, secret);
+		print(response);
+
+		sendSTARTENC();  // End encryption
+		// --- VIEW PRIVATE MESSAGES END
+
+
+		// LOGOUT
+		sendLOGOUT();
+		socketClient.close();
+
 	}
 
-	/**
-	 * This method is used to send USER command with username and get the response
-	 * from the server
-	 * 
-	 * @param String username numA This is the first parameter
-	 * @return boolean if the username is OK or INVALID
-	 */
-	public static boolean sendUserCommand(String usernameClient) {
-		writer.println(usernameClient);
-		String[] response = { "OK", "INVALID" };
-		try {
-			String responseFromServer = fromServer.readLine();
-			System.out.println(responseFromServer);
-			return responseFromServer.contains(response[0]) && !responseFromServer.contains(response[1]);
-		} catch (IOException exc) {
-			System.out.println("About Username:" + exc.getMessage());
-		}
-		return false;
+	private static void sendLOGOUT() {
+		// TODO Auto-generated method stub
+
 	}
 
-	/**
-	 * This method is used to send PASS command with password and get the response
-	 * from the server
-	 * 
-	 * @param String password This is the first parameter
-	 * @return boolean if the password is OK or INVALID
-	 */
-	public static boolean sendPassCommand(String passwordClient) {
-		writer.println(passwordClient);
-		String[] response = { "OK", "INVALID" };
-		try {
-			String responseFromServer = fromServer.readLine();
-			System.out.println(responseFromServer);
-			return responseFromServer.contains(response[0]) && !responseFromServer.contains(response[1]);
-		} catch (IOException exc) {
-			System.out.println("About Password:" + exc.getMessage());
-		}
-		return false;
+	private static void sendPRIVATE() {
+		// TODO Auto-generated method stub
+
 	}
 
-	// KEEPING THE VERSION, JUST IN CASE
-	public int getVersionOfClient() {
-		return this.version;
+	private static String decodeUS_ASCII(byte[] data) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public void setVersionOfClient(int versionCurrent) {
-		this.version = versionCurrent;
+	private static void sendPUBLIC() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void print(String response) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static byte[] receiveRESPONSE() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void sendAUTH(byte[] authEncrypted) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void sendSTARTENC() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void sendSECRET(byte[] secretEncrypted) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static byte[] getPK(byte[] cert) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static String getCA(byte[] cert) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static byte[] getSignature(byte[] cert) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static byte[] receiveHELLO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void sendHELLO() {
+		// TODO Auto-generated method stub
+
 	}
 }
